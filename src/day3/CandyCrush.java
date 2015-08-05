a806526eb29c2ce6e2f2c694adacfc22c7467b1d
kronos4462 <Guest@WTS1.attlocal.net>
2015-08-05 15:00:19 -0700

    Cameron day 3

--- src/day3/CandyCrush.java
+++ src/day3/CandyCrush.java
@@ -6,9 +6,9 @@ public class CandyCrush {
 
 	public static void main(String[] args) {
 		// 1. Using a pop-up, ask the user who they don't like
-
+String answer = JOptionPane.showInputDialog("Who do you hate");
 		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
-		JOptionPane.showMessageDialog(null, "You totally have a crush on Java!");
+		JOptionPane.showMessageDialog(null, "You totally have a crush on "+answer);
 		// 3. Ask the user for the name of their best friend
 
 		// 4. Tell them their best friend is as sweet as candy
